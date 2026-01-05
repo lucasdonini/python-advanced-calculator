363fdcc5b0f55a900d578fe04c3e57b636755698
lucasdonini <lkd.lucas@gmail.com>
2026-01-05 01:28:35 -0300

    del(tests): remove all tests

--- tests/test__input_helpers.py
+++
@@ -1,7 +0,0 @@
-from input_helpers import *
-
-
-def test__normalize_unicode_exponents__successfull():
-    raw = "3x²¹"
-    normalized = normalize_unicode_powers(raw)
-    assert normalized == "3x^21"
