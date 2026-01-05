02522a58ee6cb463d1f921f4bffff460e4d0cc90
lucasdonini <lkd.lucas@gmail.com>
2026-01-04 21:36:43 -0300

    feat(normalization): add unicode power normalizer

---
+++ input-helpers.py
@@ -0,0 +1,24 @@
+from typing import Callable
+import re
+
+
+def normalize_unicode_powers(expression: str) -> str:
+    EXPONENTS = {
+        "⁰": "0",
+        "¹": "1",
+        "²": "2",
+        "³": "3",
+        "⁴": "4",
+        "⁵": "5",
+        "⁶": "6",
+        "⁷": "7",
+        "⁸": "8",
+        "⁹": "9",
+    }
+
+    transformation: Callable[[re.Match], str] = lambda match: "^" + "".join(
+        match.group()
+    )
+
+    regex = r"[⁰¹²³⁴⁵⁶⁷⁸⁹]+"
+    return re.sub(regex, transformation, expression)
